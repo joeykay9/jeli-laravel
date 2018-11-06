0b42765ead5a0513dfaa127db406f2d423d4c445
Joel Klo <joel.klo@akaditi.com>
2018-11-06 16:59:32 +0000

    testing diamond attached to jelion

--- app/Transformers/MembersTransformer.php
+++ app/Transformers/MembersTransformer.php
@@ -29,7 +29,7 @@ class MembersTransformer extends TransformerAbstract
 							? (
 								($myself) 
 								? 'You' 
-								: $member->jelion
+								:  ('\u2666 ' . $member->jelion)
 							) : $user->pivot->contact_name),
 			'contact_phone' => $member->phone,
 			'avatar' => $member->avatar,
