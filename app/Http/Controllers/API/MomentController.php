c253fa82909c3dba282900fc536af8d7343f7751
Joel Klo <joeykay9@gmail.com>
2018-12-19 11:30:00 +0000

    debugging

--- app/Http/Controllers/API/MomentController.php
+++ app/Http/Controllers/API/MomentController.php
@@ -120,7 +120,7 @@ class MomentController extends ApiController
         if ($request->filled('schedule')) {
             foreach ($request['schedule'] as $schedule) {
                 $schedule['moment_id'] = $moment->id;
-                Schedule::create($schedule);
+                // Schedule::create($schedule);
             }
         }
 
