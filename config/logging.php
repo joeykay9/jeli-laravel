ab6e0ad9aa3ef0fafeb6d1df62ffec552552e49b
Joel Klo <joeykay9@gmail.com>
2019-01-05 12:56:39 +0000

    use errorlog

--- config/logging.php
+++ config/logging.php
@@ -36,8 +36,8 @@ return [
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+            // 'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
